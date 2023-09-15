a9a294e725c3cc6cb195f6a619360467608cf26c
Reginaldo da Silva Borges <reginaldo.borges@totvs.com.br>
2023-09-15 08:30:05 -0300

    lifecycle DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,20 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <app-title title="Para passar no lifecycle OnChange"></app-title>
+    <button (click) = "Adicionar()">Adicionar</button>
+    {{ valor }}
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
+
+  public valor:number = 1;
 
   constructor(){ }
 
   ngOnInit(): void {
-    setTimeout( () => {
-      console.log("Passou no OnInit");
-    }, 5000);
+    // setTimeout( () => {
+       console.log("Passou no OnInit");
+    // }, 5000);
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoChecked");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+
+  public Adicionar():number {
+    return this.valor += 1;
   }
 
 }
--- meu-primeiro-projeto/src/app/title/title.component.ts
+++ meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -9,12 +9,12 @@ export class TitleComponent implements OnInit, OnChanges {
 
 @Input() public title:string = "Bem vindo";
 
+  ngOnInit(): void {
+  }
+
   constructor() { }
   ngOnChanges(): void {
     console.log("Passou no OnChange");
-  }
-
-  ngOnInit(): void {
   }
 
 }
