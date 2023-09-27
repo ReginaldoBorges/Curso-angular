71ca054569783b76f5610ad48a0043370734bc66
Reginaldo da Silva Borges <reginaldo.borges@totvs.com.br>
2023-09-27 08:14:39 -0300

    Entendo melhro o uso da diretiva ngFor

--- meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -9,11 +9,19 @@ export class DiretivasEstruturaisComponent{
 
   public condition: boolean = true;
   public conditionClick: boolean = true;
+  public list: Array<{nome:string, idade:number}> = [
+    {nome: "Reginaldo", idade: 43},
+    {nome: "Borges", idade: 34},
+  ];
 
   constructor(){}
 
   ngOnInit():void {
 
+    /**
+     * Utilizando a propriedade setInterval, com auxiliar, 
+     * para alternar o valor da condição a cada 2 segundos.
+     */
     setInterval( () => {
       if (this.condition) {
         this.condition = false;
@@ -23,12 +31,29 @@ export class DiretivasEstruturaisComponent{
     }, 2000)
   }
 
+  /**
+   * Função chamada do botão Clique para alternar o valor da condição.
+   */
   public onClick(){
     if (this.conditionClick) {
       this.conditionClick = false;
     } else{
       this.conditionClick = true;
     }
+  }
+
+  /**
+   * Função chamada do botão Adicionar para inserir novo elemento à lista
+   */
+  public onClickAddList() {
+    this.list.push({nome: "Paulo", idade: 35});
+  }
+
+  /**
+   * Função chamada do envento click do item para remover intem da lista
+   */
+  public onClickEventList(event: number) {
+    this.list.splice(event, 1);
   }
 
 }
