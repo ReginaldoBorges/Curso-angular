c7c7c7b16d38ab7199852f3454ade69a2a097500
reginaldo borges <reginaldo.borges07@gmail.com>
2023-09-30 19:29:30 -0300

    Entendendo melhor o ngTemplate e o ngContent

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -9,7 +9,19 @@ import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, Afte
   <!--
     <app-diretivas-estruturais></app-diretivas-estruturais>
   -->
-  <app-diretivas-atributos></app-diretivas-atributos>
+
+  <app-diretivas-atributos>
+    <h1>Aulas de Diretivas Atributo</h1>
+    <h3>Final da aula</h3>
+  </app-diretivas-atributos>
+
+  <hr color="red">
+
+  <app-diretivas-atributos>
+    <h1>Reginaldo</h1>
+    <h3>Borges</h3>
+  </app-diretivas-atributos>
+  
     <router-outlet></router-outlet>
   `
 })
