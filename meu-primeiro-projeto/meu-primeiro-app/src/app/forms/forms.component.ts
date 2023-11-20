b6d41ccc3a6231ba5e469d52d779e617efea7d7a
ReginaldoBorges <reginaldo.borges07@gmail.com>
2023-11-20 08:32:38 -0300

    Secao_10_65 ngForm + input selected

--- meu-primeiro-projeto/meu-primeiro-app/src/app/forms/forms.component.ts
+++ meu-primeiro-projeto/meu-primeiro-app/src/app/forms/forms.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  public listComidas: Array<{ comida: string; preco: number }> = [
+    { comida: 'X-Salada', preco: 10.0 },
+    { comida: 'X-Bacon', preco: 11.0 },
+    { comida: 'Coxinha', preco: 12.0 },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
